026fcc083308c7ad5efcf6f202f34dde6488aead
Eugene Kashida <ekashida@gmail.com>
2024-02-19 08:13:42 -0800

    chore: more examples

--- force-app/main/default/lwc/importNativeWebComponent/importRegistration.js
+++ force-app/main/default/lwc/importNativeWebComponent/importRegistration.js
@@ -1,3 +1,14 @@
+class BundleRegistration extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ mode: 'closed' });
+        const p = document.createElement('p');
+        p.textContent = 'Bundle registration upgraded successfully!';
+        this._shadowRoot.appendChild(p);
+    }
+}
+customElements.define('bundle-registration', BundleRegistration);
+
 class ImportRegistration extends HTMLElement {
     constructor() {
         super();
--- force-app/main/default/lwc/inlineNativeWebComponent/inlineNativeWebComponent.js
+++ force-app/main/default/lwc/inlineNativeWebComponent/inlineNativeWebComponent.js
@@ -10,4 +10,45 @@ customElements.define('inline-registration', class extends HTMLElement {
     }
 });
 
-export default class InlineNativeWebComponent extends LightningElement {}
+export default class InlineNativeWebComponent extends LightningElement {
+    constructor() {
+        super();
+        if (!customElements.get('constructor-registration')) {
+            customElements.define('constructor-registration', class extends HTMLElement {
+                constructor() {
+                    super();
+                    this._shadowRoot = this.attachShadow({ mode: 'closed' });
+                    const p = document.createElement('p');
+                    p.textContent = 'Constructor registration upgraded successfully!';
+                    this._shadowRoot.appendChild(p);
+                }
+            });
+        }
+    }
+    connectedCallback() {
+        if (!customElements.get('connected-registration')) {
+            customElements.define('connected-registration', class extends HTMLElement {
+                constructor() {
+                    super();
+                    this._shadowRoot = this.attachShadow({ mode: 'closed' });
+                    const p = document.createElement('p');
+                    p.textContent = 'Connected registration upgraded successfully!';
+                    this._shadowRoot.appendChild(p);
+                }
+            });
+        }
+    }
+    renderedCallback() {
+        if (!customElements.get('rendered-registration')) {
+            customElements.define('rendered-registration', class extends HTMLElement {
+                constructor() {
+                    super();
+                    this._shadowRoot = this.attachShadow({ mode: 'closed' });
+                    const p = document.createElement('p');
+                    p.textContent = 'Rendered registration upgraded successfully!';
+                    this._shadowRoot.appendChild(p);
+                }
+            });
+        }
+    }
+}
